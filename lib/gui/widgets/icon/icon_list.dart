f6acfec7c219096f12e63005184edee9b50af07f
ArielM24 <ariel_montoya15@outlook.es>
2022-05-25 11:43:03 -0500

    icon list

--- lib/gui/widgets/icon/icon_list.dart
+++ lib/gui/widgets/icon/icon_list.dart
@@ -11,21 +11,30 @@ class IconList extends StatelessWidget {
     return ListView.builder(
         itemCount: iconNames.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(
-              IconNames.icons[iconNames[index]],
-              color: Colors.blue,
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.black.withOpacity(0.2))),
+              child: ListTile(
+                leading: Icon(
+                  IconNames.icons[iconNames[index]],
+                  color: Colors.blue[900],
+                ),
+                title: Text(iconNames[index]),
+                trailing: Icon(Icons.arrow_forward_ios_outlined,
+                    color: Colors.blue[900]),
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return EditorPage(
+                        iconData: IconNames.icons[iconNames[index]]!,
+                        iconName: iconNames[index]);
+                  }));
+                },
+              ),
             ),
-            title: Text(iconNames[index]),
-            subtitle: const Divider(),
-            trailing: const Icon(Icons.arrow_forward_ios_outlined),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return EditorPage(
-                    iconData: IconNames.icons[iconNames[index]]!,
-                    iconName: iconNames[index]);
-              }));
-            },
           );
         });
   }
